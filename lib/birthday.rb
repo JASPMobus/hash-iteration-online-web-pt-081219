73db23dc9c91b41db9c71892c3ccd8b52d5eecb0
James Mobus <mobusj@live.com>
2019-08-21 22:21:59 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,7 +8,7 @@ def happy_birthday(birthday_kids)
   # add your code snippet here!
   
   birthday_kids.each do |kid|
-    puts "Happy #{birthday_kids[kid]}th birthday, #{kid}!"
+    puts "Happy Birthday #{kid[0]}! You are now #{kid[1]} years old!"
   end
 end
 
